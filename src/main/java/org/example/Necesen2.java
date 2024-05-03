81bdccc460ac0ef52f19f90462d6ab030ff6e5cb
aybeniz <aybenizkazimova15@gmail.com>
2024-05-03 15:42:03 +0400

    new repo

--- src/main/java/org/example/Necesen2.java
+++ src/main/java/org/example/Necesen2.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Necesen2 {
+    static int a = 5;
+
+
     public static void main(String[] args) {
-        System.out.println("Ejdahalarrrrrrrr");
+        try {
+            getAgain("aa");
+        } catch (Exception e) {
+            System.out.println("aaa");
+        }
+    }
+
+    public static void getAgain(String file) throws IOException {
+        Main.getFile("Hello");
     }
 }
