9d3d25a1bfe4ad1ddc837ee3987baf03a8c5d3d5
aybeniz <aybenizkazimova15@gmail.com>
2024-05-05 12:37:55 +0400

    DAO

--- src/main/java/org/example/dao/DAO.java
+++ src/main/java/org/example/dao/DAO.java
@@ -1,4 +1,15 @@
 package org.example.dao;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Predicate;
 
-public interface DAO {
+public interface DAO <T>{
+    T save(T t);
+
+  Collection<T> getAll();
+
+  Optional<T> findOneBy(Predicate<T> predicate);
+
+    Collection<T> findAllBy(Predicate<T> predicate);
+
 }
