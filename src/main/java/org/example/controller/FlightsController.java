c48810f8e267917df5a4cfc81aa3f0acceb00505
aybeniz <aybenizkazimova15@gmail.com>
2024-05-09 13:03:00 +0400

    DAO

--- src/main/java/org/example/controller/FlightsController.java
+++ src/main/java/org/example/controller/FlightsController.java
@@ -1,6 +1,9 @@
 package org.example.controller;
 
+import org.example.entities.FlightsEntity;
 import org.example.service.FlightsService;
+
+import java.util.*;
 
 public class FlightsController  {
 
@@ -10,8 +13,18 @@ public class FlightsController  {
     }
 
 
-    public String displayOnlineBoard(){
-        return null;
+    public  void displayOnlineBoard(){
+            List<FlightsEntity> flights = flightsService.getAllFlightsFromKievNext24Hours();
+
+            System.out.println("Flights from Kiev in the next 24 hours:");
+            System.out.println("--------------------------------------");
+            if (flights.isEmpty()) {
+                System.out.println("No flights available from Kiev in the next 24 hours.");
+            } else {
+                for (FlightsEntity flight : flights) {
+                    System.out.println(flight); // Assuming Flight class has appropriate toString() method
+                }
+            }
     }
     public String searchBookFlight(){
         return null;
