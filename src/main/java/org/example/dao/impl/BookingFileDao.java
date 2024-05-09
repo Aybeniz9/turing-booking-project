9bbb5634771b7c65e4deb99a8ac40c742728a90c
aybeniz <aybenizkazimova15@gmail.com>
2024-05-09 13:07:22 +0400

    DAO

--- src/main/java/org/example/dao/impl/BookingFileDao.java
+++ src/main/java/org/example/dao/impl/BookingFileDao.java
@@ -1,8 +1,5 @@
 package org.example.dao.impl;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.example.dao.BookingDao;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.model.BookingEntity;
 
 public class BookingFileDao  extends BookingDao {
     
