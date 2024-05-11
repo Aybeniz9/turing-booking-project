39374dc7839229a15a0a25d5b8871d262dcd3210
aybeniz <aybenizkazimova15@gmail.com>
2024-05-11 19:04:57 +0400

    Refactor

---
+++ src/main/test/BookingServiceİmplTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceİmplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createBooking() {
+    }
+
+    @Test
+    void cancelBooking() {
+    }
+
+    @Test
+    void getAllBookings() {
+    }
+
+    @Test
+    void getMyFlights() {
+    }
+
+    @Test
+    void findBookingByOne() {
+    }
+}
